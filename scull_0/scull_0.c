38e4ad1fb6a8afb25142f3dcd6001149cb704d66
JohnMushatt <john.mushatt@gmail.com>
2020-03-14 23:13:56 -0400

    Added char driver write function:

--- scull_0/scull_0.c
+++ scull_0/scull_0.c
@@ -49,7 +49,7 @@ int scull_open(struct inode *inode, struct file *filp) {
     return 0;
 }
 
-int scull_release(struct *inode, struct fule *filp) {
+int scull_release(struct *inode, struct file *filp) {
     return 0;
 
 }
@@ -77,37 +77,102 @@ int scull_trim(struct scull_dev *dev) {
     dev->data = NULL;
     return 0;
 }
-int scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos) {
-    struct scull_dev *dev = filp->private_data;
-    //Current list item
-    struct scull_qset *dptr;
-    int quantum, qset;
-    //# of bytes in the list stream
-    int itemsize;
+ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos) {
+	struct scull_dev *dev = filp->private_data;
+	struct scull_qset *dptr;	/* the first listitem */
+	int quantum = dev->quantum, qset = dev->qset;
+	int itemsize = quantum * qset; /* how many bytes in the listitem */
+	int item, s_pos, q_pos, rest;
+	ssize_t retval = 0;
 
-    int item, s_pos, q_pos, rest;
-    ssize_t retval = 0;
+	if (down_interruptible(&dev->sem))
+		return -ERESTARTSYS;
+	if (*f_pos >= dev->size) {
+		goto out;
+  }
+	if (*f_pos + count > dev->size) {
+		count = dev->size - *f_pos;
+  }
+	/* find listitem, qset index, and offset in the quantum */
+	item = (long)*f_pos / itemsize;
+	rest = (long)*f_pos % itemsize;
+	s_pos = rest / quantum; q_pos = rest % quantum;
 
-    if(down_interruptible(&dev->sem)) {
-      return -ERESTARTSYS;
+	/* follow the list up to the right position (defined elsewhere) */
+	dptr = scull_follow(dev, item);
+
+	if (dptr == NULL || !dptr->data || ! dptr->data[s_pos])
+		goto out; /* don't fill holes */
+
+	/* read only up to the end of this quantum */
+	if (count > quantum - q_pos)
+		count = quantum - q_pos;
+
+	if (copy_to_user(buf, dptr->data[s_pos] + q_pos, count)) {
+		retval = -EFAULT;
+		goto out;
+	}
+	*f_pos += count;
+	retval = count;
+
+  out:
+	   up(&dev->sem);
+	   return retval;
+}
+ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos) {
+  struct scull_dev *dev = filp->private_data;
+  struct scull_qset *dptrl
+  int quantum = dev->quantum, qset = dev->qset;
+  int itemsize = quantum * qset;
+
+  int item, s_pos, q_pos, rest;
+
+  ssize_t retval = -ENOMEM;
+
+  if(down_interruptible(&dev->sem)) {
+    return -ERESTARTSYS;
+  }
+  item = (long) *f_pos / itemsize;
+  rest = (long)*f_pos % itemsize;
+
+  s_pos = rest / quantum;
+  q_pos = rest % quantum;
+
+  dptr = scull_follow(dev,item);
+
+  if(!dptr == NULL) {
+    goto out;
+  }
+
+  if(!dptr->data) {
+    dptr->data = kmalloc(qset * sizeof(char *), GFP_KERNEL);
+    /* Check if memory was allocated in kernel space */
+    if(!dptr->data) {
+      goto out;
     }
-    else {
-      quantum = dev->quantum;
-      qset = dev->qset;
-
-      itemsize = quantum * qset;
-
-      if(*f_pos >= dev->size) {
-        goto out;
-      }
-      if(*f_pos + count > dev->size) {
-        count = dev->size - *f_pos;
-      }
-
-      item = (long) *f_pos / itemsize;
-      rest = (long) *f_pos % itemsize;
-
-      s_pos = rest / quantum;
-      q_pos = rest % quantum;
+    memset(dptr->data,0,qset *sizeof(char *));
+  }
+  if(!dptr->data[s_pos]) {
+    dptr->data[s_pos] = kmalloc(quantum,GFP_KERNEL);
+    if(!dptr->data[s_pos]) {
+      goto out;
     }
+  }
+  if(count > quantum - q_pos) {
+    count = quantum - q_pos;
+  }
+  if(copy_from_user(dptr->data[s_pos]+q_pos, buf,count)) {
+    retval = -EFAULT;
+    goto out;
+  }
+  *f_pos +=count;
+  /* Return the number of bytes written */
+  retval = count;
+
+  if(dev->size < *f_pos) {
+    dev->size = *f_pos;
+  }
+  out:
+    up(&dev->sem);
+    return retval;
 }
